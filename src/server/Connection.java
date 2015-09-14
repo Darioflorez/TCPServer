6f74e73ebe6883f0d497a8a7f5fa3f8709c93006
darioflorez <florez.wilmar@gmail.com>
2015-09-15 00:39:44 +0200

    Server split in functions

--- src/server/Connection.java
+++ src/server/Connection.java
@@ -26,33 +26,76 @@ public class Connection extends Thread{
         }
     }
     public void run(){
+        System.out.println("Client Connected!!");
         //While Client Active
-        String data;
-        while(clientActive){
-            System.out.println("Client Connected!!");
-            try{
-                //Listen for new messages
-                while ((data = in.readUTF()) != null) {
-                    System.out.println("> " + data);
-                    //send back to the client
-                    out.writeUTF(data);
+        String msg;
+        if(respond("welcome")){
+            while(clientActive){
+                msg = getMessage();
+                if(msg != null){
+                    System.out.println("> " + msg);
+                    evalMessage(msg);
                 }
-            }catch(IOException e){
-                System.out.println("readLine " + e.getMessage());
-                closeConnection();
-            }finally {
-                try{
-                    clientSocket.close();
-                }catch(IOException e){
-                    System.out.println("Close connection" + e.getMessage());
+                else{
+                    closeConnection();
                 }
             }
         }
+        else{
+            closeConnection();
+        }
+    }
+
+    protected String getMessage(){
+        String msg = null;
+        try {
+            msg = in.readUTF();
+        }catch(IOException e){
+            System.out.println("getMessage: " + e.getMessage());
+        }
+        return msg;
     }
 
     public void closeConnection(){
-        clientActive = false;
-        System.out.println("Client bye!!");
+        try{
+            clientActive = false;
+            clientSocket.close();
+            System.out.println("Client bye!!");
+        }catch(IOException e){
+            System.out.println("Close connection: " + e.getMessage());
+        }
+    }
+
+    public boolean respond(String msg){
+        try {
+            out.writeUTF(msg);
+            return true;
+        }catch(IOException e){
+            System.out.println("Respond: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public void evalMessage(String msg){
+        if(msg.contains("/")){
+            String[] cmd = msg.split("/");
+            switch (cmd[1]){
+                case "help":
+                    respond("YOU NEED HELP!");
+                    break;
+                case "quit":
+                    closeConnection();
+                    break;
+                case "who":
+                    respond("WHO!!!");
+                    break;
+                default:
+                    respond("This is not a command!!!");
+            }
+        }else{
+            respond(msg);
+        }
+
     }
 
 
